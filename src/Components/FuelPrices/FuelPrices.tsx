6adf27a1288e62853e0fbf8aae0de8d6a3c37258
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-08-18 19:30:19 +0300

    removed console.logs

--- src/Components/FuelPrices/FuelPrices.tsx
+++ src/Components/FuelPrices/FuelPrices.tsx
@@ -6,7 +6,6 @@ export function FuelPrices() {
   const [petrolPrice, setPetrolPrice] = useState<null | number | string>(null);
   const [dieselPrice, setDieselPrice] = useState<null | number | string>(null);
   const [loading, setLoading] = useState(true);
-  console.log(exchangeKey, fueloKey);
 
   useEffect(() => {
     async function fetchPrices() {
@@ -16,7 +15,6 @@ export function FuelPrices() {
           `https://api.freecurrencyapi.com/v1/latest?apikey=${exchangeKey}&currencies=RON&base_currency=BGN`
         );
         const exchangeData = await exchangeRes.json();
-        console.log(exchangeData);
 
         const petrolResponse = await fetch(
           `https://fuelo.net/api/price?key=${fueloKey}&fuel=gasoline&date=2025-08-08`
@@ -34,9 +32,6 @@ export function FuelPrices() {
         setDieselPrice(
           Number(dieselData.price * exchangeData.data.RON).toFixed(2)
         );
-        console.log(dieselData);
-        console.log(petrolData);
-        console.log(Date.now());
       } catch (error) {
         console.error("Error fetching prices:", error);
       } finally {
